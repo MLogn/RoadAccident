5fb3981935ed4778257b7675f2465077b3879847
Mikhail <loginoffmal@gmail.com>
2018-12-18 02:04:51 +0300

    sql requests --done

--- MySite/urls.py
+++ MySite/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('edit/<int:id>/', views.edit, name='edit'),
     path('delete/<int:id>/', views.delete, name='delete'),
-    path('stat.html>/', views.stat, name='stat'),
+    path('stat.html', views.stat, name='stat'),
 ]
